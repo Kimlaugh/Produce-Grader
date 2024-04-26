c3b76a2d30237e698a29bd364c3761852daa07d9
Calvin Stephenson <cstephenson882@gmail.com>
2024-04-25 21:00:27 -0500

    update 25/2024

--- BlemishClassifier/blemish.py
+++ BlemishClassifier/blemish.py
@@ -1,174 +1,129 @@
 import cv2
 import numpy as np
 import matplotlib.pyplot as plt
+from PIL import Image
 
 
-def identify_outline(img):
+def save_image(img_, name_):
+    image_name = name_ + '.png'
+    image_pil = Image.fromarray(img_)
+    image_pil.save(image_name)
 
-  LAB = cv2.cvtColor(img, cv2.COLOR_BGR2LAB)
 
-  # Extract hue and saturation channels
-  L, a, b = cv2.split(LAB)
+def show_image(image_, description, save_=False):
+    # Convert BGR to RGB for saving with PIL
+    image_rgb = cv2.cvtColor(image_, cv2.COLOR_BGR2RGB)
 
-  # Threshold the hue channel to identify the region of interest
-  _, thresholded = cv2.threshold(a, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
+    # Plot the image using matplotlib
+    plt.imshow(image_rgb)
+    plt.title(description)
+    plt.axis('off')
+    plt.show()
 
-  # Find contours
-  contours, _ = cv2.findContours(thresholded, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-
-  # Draw contours on the original image
-  contour_img = img.copy()
-  return contours
+    # Save the image if required
+    if save_:
+        save_image(image_, description)
 
 
 def draw_contours(img):
-  contours = identify_outline(img)
-  # Find the contour with the largest area
-  main_contour = max(contours, key=cv2.contourArea)
+    LAB = cv2.cvtColor(img, cv2.COLOR_BGR2LAB)
 
-  # Create a mask for segmented pixels using the main contour
-  mask = np.zeros_like(img)
-  cv2.drawContours(mask, [main_contour], -1, (255, 255, 255), cv2.FILLED)
+    # Extract hue and saturation channels
+    L, a, b = cv2.split(LAB)
 
-  # Apply the mask to the original image
-  segmented_img = cv2.bitwise_and(img, mask)
+    # Threshold the hue channel to identify the region of interest
+    _, thresholded = cv2.threshold(a, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
 
-  # Create a mask for contour outlines using the main contour
-  contour_outline_mask = np.zeros_like(img)
-  cv2.drawContours(contour_outline_mask, [main_contour], -1, (0, 255, 0), thickness=8)
+    # Find contours
+    contours, _ = cv2.findContours(thresholded, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 
-  # Combine contour outlines with original image
-  image_with_contour = cv2.addWeighted(img, 1, contour_outline_mask, 1, 0)
+    # Draw contours on the original image
+    contour_img = img.copy()
 
-  # Convert BGR to RGB for matplotlib
-  segmented_img_rgb = cv2.cvtColor(segmented_img, cv2.COLOR_BGR2RGB)
-  image_with_contour_rgb = cv2.cvtColor(image_with_contour, cv2.COLOR_BGR2RGB)
+    # Find the contour with the largest area
+    main_contour = max(contours, key=cv2.contourArea)
 
-  # Threshold the grayscale image to obtain only the segmented pixels
-  segmented_gray = cv2.cvtColor(segmented_img, cv2.COLOR_BGR2GRAY)
-  _, segmented_pixels = cv2.threshold(segmented_gray, 1, 255, cv2.THRESH_BINARY)
+    # Create a mask for segmented pixels using the main contour
+    mask = np.zeros_like(img)
+    cv2.drawContours(mask, [main_contour], -1, (255, 255, 255), cv2.FILLED)
 
-  # Plot the images
-  plt.figure(figsize=(18, 6))
+    # Apply the mask to the original image
+    segmented_img = cv2.bitwise_and(img, mask)
 
-  # Plot the original image with contour outlines
-  # plt.subplot(1, 3, 1)
-  # plt.imshow(image_with_contour_rgb)
-  # plt.title('Original Image with Contour Outline')
-  # plt.axis('off')
+    # Create a mask for contour outlines using the main contour
+    contour_outline_mask = np.zeros_like(img)
+    cv2.drawContours(contour_outline_mask, [main_contour], -1, (0, 255, 0), thickness=8)
 
-  # # Plot the original image with only segmented pixels
-  # plt.subplot(1, 3, 2)
-  # plt.imshow(segmented_pixels, cmap='gray')
-  # plt.title('Original Image with Segmented Pixels')
-  # plt.axis('off')
+    # Combine contour outlines with original image
+    image_with_contour = cv2.addWeighted(img, 1, contour_outline_mask, 1, 0)
 
-  # # Plot the segmented image
-  # plt.subplot(1, 3, 3)
-  # plt.imshow(segmented_img_rgb)
-  # plt.title('Segmented Image')
-  # plt.axis('off')
+    # Convert BGR to RGB for matplotlib
+    segmented_img_rgb = cv2.cvtColor(segmented_img, cv2.COLOR_BGR2RGB)
+    image_with_contour_rgb = cv2.cvtColor(image_with_contour, cv2.COLOR_BGR2RGB)
 
-  # plt.show()
-  return segmented_img_rgb
+    return segmented_img_rgb
 
 
-# Convert the segmented image to grayscale
+def total_pixel_count(img_rgb):
+    # Convert segmented image from RGB to grayscale
+    segmented_img_gray = cv2.cvtColor(img_rgb, cv2.COLOR_RGB2GRAY)
 
-def total_pixel_count(segmented_img_rgb):
-
-  segmented_img_gray = cv2.cvtColor(segmented_img_rgb, cv2.COLOR_RGB2GRAY)
-
-  # Count the number of non-zero pixels (pixels that are not black)
-  total_num_pixels = np.count_nonzero(segmented_img_gray)
-
-  # print("Number of segmented pixels:", total_num_pixels)
-  # plt.subplot(1, 3, 3)
-  # plt.imshow(segmented_img_gray)
-  # plt.title('Segmented Image Gray')
-  # plt.axis('off')
-
-  # plt.show()
-  #print(segmented_img_gray)
-  return total_num_pixels
+    # Count the number of non-zero pixels (pixels that are not black)
+    total_num_pixels = np.count_nonzero(segmented_img_gray)
+    return total_num_pixels
 
 
+def image_blemish_mask(img_rgb, produce_type):
+    # Convert segmented image from RGB to HSV color space
+    segmented_img_hsv = cv2.cvtColor(img_rgb, cv2.COLOR_RGB2HSV)
+
+    # Define lower and upper threshold values for tomato red in HSV color space
+    if produce_type == "tomato":
+        lower_gray = np.array([0, 0, 0])  # Lower threshold for low saturation and mid-value
+        upper_gray = np.array([256, 200, 200])
+    elif produce_type == 'carrot':
+        lower_gray = np.array([0, 0, 0])  # Lower threshold for low saturation and mid-value
+        upper_gray = np.array([256, 200, 200])
+    else:
+        lower_gray = np.array([0, 0, 20])  # Lower threshold for low saturation and mid-value
+        upper_gray = np.array([180, 250, 180])
+
+    # Create a mask based on the threshold values
+    mask = cv2.inRange(segmented_img_hsv, lower_gray, upper_gray)
+
+    # Apply the mask to the segmented image
+    segmented_img_blemish = cv2.bitwise_and(img_rgb, img_rgb, mask=mask)
+
+    # Convert to grayscale
+    segmented_img_gray = cv2.cvtColor(segmented_img_blemish, cv2.COLOR_RGB2GRAY)
+
+    # Count the number of non-zero pixels (pixels that are not black)
+    black_spot_num_pixels = np.count_nonzero(segmented_img_gray)
+
+    return black_spot_num_pixels, segmented_img_blemish
 
 
-def image_blemish_mask(segmented_img_rgb, produce_type):
+def blemish_percent(imgs):
+    total_percentage = 0
+    produce_type = 'carrot'
+
+    # Read the image once outside the loop
+    img = cv2.imread(imgs[0])
+    segmented_img = draw_contours(img)
+
+    for img_path in imgs:
+        # Read image data
+        img = cv2.imread(img_path)
+
+        # Process the image and calculate blemish percentage
+        blemish_pixels, blemish_img = image_blemish_mask(segmented_img, produce_type)
+        total_percentage += (blemish_pixels / total_pixel_count(segmented_img)) * 100
+
+    # Calculate the average percentage
+    average_percentage = total_percentage / len(imgs)
+
+    return average_percentage
 
 
-  # Convert segmented image from RGB to HSV color space
-  segmented_img_hsv = cv2.cvtColor(segmented_img_rgb, cv2.COLOR_RGB2HSV)
-
-  # Define lower and upper threshold values for tomato red in HSV color space
-  if produce_type == "tomato":
-    lower_gray = np.array([0, 0, 0])  # Lower threshold for low saturation and mid-value
-    upper_gray = np.array([256, 200, 200 ])
-    print("tomato")
-  elif produce_type == 'carrot':
-    lower_gray = np.array([0, 0, 0])  # Lower threshold for low saturation and mid-value
-    upper_gray = np.array([256, 200, 200 ])
-  else:
-    lower_gray = np.array([0, 0, 20])  # Lower threshold for low saturation and mid-value
-    upper_gray = np.array([180, 250, 180])
-
-  # Create a mask based on the threshold values
-  mask = cv2.inRange(segmented_img_hsv, lower_gray, upper_gray)
-
-  # Apply the mask to the segmented image
-  segmented_img_red = cv2.bitwise_and(segmented_img_rgb, segmented_img_rgb, mask=mask)
-  ###################################
-  # Display the segmented image containing tomato red pixels (can keep this one)
-#   plt.imshow(segmented_img_red)
-#   plt.title('Segmented Image: Tomato Red Pixels')
-#   plt.axis('off')
-#   plt.show()
-  ###################################
-  segmented_img_gray = cv2.cvtColor(segmented_img_red, cv2.COLOR_RGB2GRAY)
-
-  # Count the number of non-zero pixels (pixels that are not black)
-  black_spot_num_pixels = np.count_nonzero(segmented_img_gray)
-
-  # print("Number of segmented pixels:", black_spot_num_pixels)
-
-  # plt.subplot(1, 3, 3)
-  # plt.imshow(segmented_img_gray)
-  # plt.title('Segmented Image Gray')
-  # plt.axis('off')
-
-  # plt.show()
-  # print(segmented_img_gray)
-
-  
-
-  # return segmented_img_red
-  return black_spot_num_pixels
- 
-
-# img = cv2.imread('/content/tbm.png')
-
-runs_ = 0
-percentage = 0
-produce_type = 'tomato'
-# imgages_list = ['/content/tbm.png','/content/tbm2.png', '/content/t4.png'] # for tomato
-imgages_list = [r'C:\Users\calvin\Downloads\UWI\Year 3\Semester 2\CSTONE\Appearance\Produce-Grader\Classifier\test_images\tomato\cropped_image2.png']
-
-
-
-def calculate_percentage(num_pixels, total_pixels):
-    percentage = (num_pixels / total_pixels) * 100
-    if percentage > 8:
-        print ("This tomato failed inspection for black spots")
-    return percentage
-
-for img in imgages_list:
-  img = cv2.imread(img)
-  percentage += calculate_percentage(image_blemish_mask(draw_contours(img),produce_type),total_pixel_count(draw_contours(img)))
-  runs_ += 1
-
-percentage = percentage/runs_
-print(f"\n\nblemish percent: {percentage}")
-  
-
-
+imgs = [r'C:\Users\calvin\Downloads\UWI\Year 3\Semester 2\CSTONE\Appearance\Produce-Grader\Classifier\test_images\tomato\img1.jpg'] * 100
+print(f"\nBlemish percentage: {blemish_percent(imgs)}")
---
+++ ImageClassifier/model.py
@@ -0,0 +1,111 @@
+from tensorflow.keras.applications.resnet50 import preprocess_input
+import numpy as np
+from tensorflow import keras
+from keras.preprocessing import image
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+import warnings
+warnings.filterwarnings('ignore')
+
+from keras.models import load_model
+model = keras.models.load_model(r"C:\Users\calvin\Downloads\UWI\Year 3\Semester 2\CSTONE\ImageClassifier\pretrained_model.h5")
+
+class_map = {0: 'carrot', 1: 'pepper', 2: 'tomato'}
+image_categories = ['carrot','pepper','tomato']
+
+
+#######################################################################################################
+#          Plotting Funtions
+#-----------------------------------------------------------------------------------------------------
+
+# test_gen = ImageDataGenerator() # Normalise the data
+# test_image_generator = test_gen.flow_from_directory(
+#                                             test_path,
+#                                             target_size=(150, 150),
+#                                             batch_size=32,
+#                                             class_mode='categorical')
+
+# def plot(hist):
+#     h = hist.history
+#     plt.style.use('ggplot')
+#     plt.figure(figsize=(10, 5))
+#     plt.plot(h['loss'], c='red', label='Training Loss')
+#     plt.plot(h['val_loss'], c='red', linestyle='--', label='Validation Loss')
+#     plt.plot(h['accuracy'], c='blue', label='Training Accuracy')
+#     plt.plot(h['val_accuracy'], c='blue', linestyle='--', label='Validation Accuracy')
+#     plt.xlabel("Number of Epochs")
+#     plt.legend(loc='best')
+#     plt.show()
+
+# test_gen = ImageDataGenerator() # Normalise the data
+# test_image_generator = test_gen.flow_from_directory(
+#                                             r"C:\Users\calvin\Downloads\UWI\Year 3\Semester 2\CSTONE\ImageClassifier\test_images",
+#                                             target_size=(150, 150),
+#                                             batch_size=32,
+#                                             class_mode='categorical')
+
+
+#########################################################################################################
+#                                   PREDICTIONS
+#--------------------------------------------------------------------------------------------------------  
+from keras.metrics import MeanMetricWrapper, Accuracy
+
+## Assuming you have created your model
+# model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=[Accuracy()])
+
+## Then in your evaluation function
+# test_loss, test_acc = model.evaluate(test_image_generator)
+
+image_path = r"C:\Users\calvin\Pictures\Screenshots\Screenshot 2024-03-25 090909.png"
+def predictor(img_path, loop, crop_width_height):
+    from keras.applications.vgg16 import preprocess_input
+    from keras.preprocessing import image
+    
+    try:
+        image_path = img_path
+        img = image.load_img(image_path, target_size=(150, 150))
+      
+
+        img_array = image.img_to_array(img)
+        
+        # Preprocess the image array
+        img_array = preprocess_input(img_array)
+
+        # Expand dimensions to match the expected shape (batch size, height, width, channels)
+        img_array = np.expand_dims(img_array, axis=0)
+        
+        prediction = model.predict(img_array)
+
+        # Set a threshold for prediction confidence
+        threshold = 0.75
+
+        if loop == 0:
+            return "Not identified"
+        
+        # Check if the maximum confidence is below the threshold
+        if np.max(prediction) < threshold:
+            print("The image is not close to what the model has been trained on.")
+        else:
+            # Get the class with the highest confidence
+            predicted_class = np.argmax(prediction)
+            print(f"The image is predicted as class {image_categories[predicted_class]} with confidence {prediction[0][predicted_class]}")
+
+    except Exception as e:
+        print(f"An error occurred: {e}")
+
+counter = 0
+while (counter < 5):
+    image_path = str(input("enter image path: ",))
+
+    image_path = image_path.replace("\\",'/')
+    try:
+        predictor(image_path,3,700)
+    except():
+        image_path = image_path[3:]
+    counter+= 1
+    
+
+
+
+
+# print(999999)
+    
